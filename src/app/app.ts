958d05d5594c9dbf40ce8f10d1350ec483ab795c
KinatorAr <vegetaun4343@gmail.com>
2026-05-06 14:06:28 -0600

    Eliminacion de Splash para Produccion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,22 +8,4 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('portafolio');
-
-  ngOnInit() {
-    this.hideSplashScreen();
-  }
-
-  private hideSplashScreen() {
-    const splash = document.getElementById('splash-screen');
-    if (splash) {
-      splash.style.transition = 'opacity 0.5s ease, visibility 0.5s';
-      splash.style.opacity = '0';
-      splash.style.visibility = 'hidden';
-
-      setTimeout(() => {
-        splash.remove();
-      }, 500);
-    }
-  }
 }
