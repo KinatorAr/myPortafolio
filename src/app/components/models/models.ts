9820f2d371f759a4dab50bc9951454b936a0070a
KinatorAr <vegetaun4343@gmail.com>
2026-05-14 18:16:07 -0600

    cambios carrusel proyecto Entre Ramas

--- src/app/components/models/models.ts
+++ src/app/components/models/models.ts
@@ -11,22 +11,25 @@ import { Carousel, CarouselItem } from '../../shared/carousel/carousel';
 export class Models {
   slides: CarouselItem[] = [
     {
-      imageUrl: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=900&auto=format&fit=crop',
-      title: 'Cimas del Silencio',
-      subtitle: 'Una plataforma web diseñada para amantes del senderismo con Angular.',
-      tag: 'Angular / Web',
+      imageUrl: '/img/entreRamas.webp',
+      title: 'Entre Ramas',
+      subtitle: 'Pagina Web de restaurante con menú interactivo',
+      tag: 'HTML / CSS / JS',
+      ctaUrl: 'https://entre-ramas.vercel.app/',
     },
     {
       imageUrl: 'https://images.unsplash.com/photo-1555993539-1732b0258235?w=900&auto=format&fit=crop',
       title: 'Luces de la Ciudad',
       subtitle: 'Dashboard administrativo para gestión de tráfico urbano inteligente.',
       tag: 'React / UI',
+      ctaUrl: '#',
     },
     {
       imageUrl: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=900&auto=format&fit=crop',
       title: 'Horizontes de Arena',
       subtitle: 'E-commerce de viajes con integración de pagos en tiempo real.',
       tag: 'Fullstack',
+      ctaUrl: '#',
     }
   ];
 }
